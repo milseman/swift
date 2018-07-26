05f307fdb094a25bce02b6d16e31b43b4c2b5647
Michael Ilseman <milseman@apple.com>
2018-07-26 10:07:18 -0700

    WIP: move printing code into Print.swift from OutputStream.swift

--- stdlib/public/core/Print.swift
+++ stdlib/public/core/Print.swift
@@ -261,3 +261,243 @@ internal func _debugPrint<Target : TextOutputStream>(
   }
   output.write(terminator)
 }
+
+//===----------------------------------------------------------------------===//
+// Default (ad-hoc) printing
+//===----------------------------------------------------------------------===//
+
+@usableFromInline // FIXME(sil-serialize-all)
+@_silgen_name("swift_EnumCaseName")
+internal func _getEnumCaseName<T>(_ value: T) -> UnsafePointer<CChar>?
+
+@usableFromInline // FIXME(sil-serialize-all)
+@_silgen_name("swift_OpaqueSummary")
+internal func _opaqueSummary(_ metadata: Any.Type) -> UnsafePointer<CChar>?
+
+/// Do our best to print a value that cannot be printed directly.
+@inline(never) // slow-path
+@_semantics("optimize.sil.specialize.generic.never")
+internal func _adHocPrint_unlocked<T, TargetStream: TextOutputStream>(
+    _ value: T, _ mirror: Mirror, _ target: inout TargetStream,
+    isDebugPrint: Bool
+) {
+  func printTypeName(_ type: Any.Type) {
+    // Print type names without qualification, unless we're debugPrint'ing.
+    target.write(_typeName(type, qualified: isDebugPrint))
+  }
+
+  if let displayStyle = mirror.displayStyle {
+    switch displayStyle {
+      case .optional:
+        if let child = mirror.children.first {
+          _debugPrint_unlocked(child.1, &target)
+        } else {
+          _debugPrint_unlocked("nil", &target)
+        }
+      case .tuple:
+        target.write("(")
+        var first = true
+        for (label, value) in mirror.children {
+          if first {
+            first = false
+          } else {
+            target.write(", ")
+          }
+
+          if let label = label {
+            if !label.isEmpty && label[label.startIndex] != "." {
+              target.write(label)
+              target.write(": ")
+            }
+          }
+
+          _debugPrint_unlocked(value, &target)
+        }
+        target.write(")")
+      case .struct:
+        printTypeName(mirror.subjectType)
+        target.write("(")
+        var first = true
+        for (label, value) in mirror.children {
+          if let label = label {
+            if first {
+              first = false
+            } else {
+              target.write(", ")
+            }
+            target.write(label)
+            target.write(": ")
+            _debugPrint_unlocked(value, &target)
+          }
+        }
+        target.write(")")
+      case .enum:
+        if let cString = _getEnumCaseName(value),
+            let caseName = String(validatingUTF8: cString) {
+          // Write the qualified type name in debugPrint.
+          if isDebugPrint {
+            printTypeName(mirror.subjectType)
+            target.write(".")
+          }
+          target.write(caseName)
+        } else {
+          // If the case name is garbage, just print the type name.
+          printTypeName(mirror.subjectType)
+        }
+        if let (_, value) = mirror.children.first {
+          if Mirror(reflecting: value).displayStyle == .tuple {
+            _debugPrint_unlocked(value, &target)
+          } else {
+            target.write("(")
+            _debugPrint_unlocked(value, &target)
+            target.write(")")
+          }
+        }
+      default:
+        target.write(_typeName(mirror.subjectType))
+    }
+  } else if let metatypeValue = value as? Any.Type {
+    // Metatype
+    printTypeName(metatypeValue)
+  } else {
+    // Fall back to the type or an opaque summary of the kind
+    if let cString = _opaqueSummary(mirror.subjectType),
+        let opaqueSummary = String(validatingUTF8: cString) {
+      target.write(opaqueSummary)
+    } else {
+      target.write(_typeName(mirror.subjectType, qualified: true))
+    }
+  }
+}
+
+@usableFromInline
+@inline(never)
+@_semantics("optimize.sil.specialize.generic.never")
+internal func _print_unlocked<T, TargetStream: TextOutputStream>(
+  _ value: T, _ target: inout TargetStream
+) {
+  // Optional has no representation suitable for display; therefore,
+  // values of optional type should be printed as a debug
+  // string. Check for Optional first, before checking protocol
+  // conformance below, because an Optional value is convertible to a
+  // protocol if its wrapped type conforms to that protocol.
+  if _isOptional(type(of: value)) {
+    let debugPrintable = value as! CustomDebugStringConvertible
+    debugPrintable.debugDescription.write(to: &target)
+    return
+  }
+  if case let streamableObject as TextOutputStreamable = value {
+    streamableObject.write(to: &target)
+    return
+  }
+
+  if case let printableObject as CustomStringConvertible = value {
+    printableObject.description.write(to: &target)
+    return
+  }
+
+  if case let debugPrintableObject as CustomDebugStringConvertible = value {
+    debugPrintableObject.debugDescription.write(to: &target)
+    return
+  }
+
+  let mirror = Mirror(reflecting: value)
+  _adHocPrint_unlocked(value, mirror, &target, isDebugPrint: false)
+}
+
+//===----------------------------------------------------------------------===//
+// `debugPrint`
+//===----------------------------------------------------------------------===//
+
+@_semantics("optimize.sil.specialize.generic.never")
+@inline(never)
+public func _debugPrint_unlocked<T, TargetStream: TextOutputStream>(
+    _ value: T, _ target: inout TargetStream
+) {
+  if let debugPrintableObject = value as? CustomDebugStringConvertible {
+    debugPrintableObject.debugDescription.write(to: &target)
+    return
+  }
+
+  if let printableObject = value as? CustomStringConvertible {
+    printableObject.description.write(to: &target)
+    return
+  }
+
+  if let streamableObject = value as? TextOutputStreamable {
+    streamableObject.write(to: &target)
+    return
+  }
+
+  let mirror = Mirror(reflecting: value)
+  _adHocPrint_unlocked(value, mirror, &target, isDebugPrint: true)
+}
+
+@usableFromInline
+@_semantics("optimize.sil.specialize.generic.never")
+internal func _dumpPrint_unlocked<T, TargetStream: TextOutputStream>(
+    _ value: T, _ mirror: Mirror, _ target: inout TargetStream
+) {
+  if let displayStyle = mirror.displayStyle {
+    // Containers and tuples are always displayed in terms of their element
+    // count
+    switch displayStyle {
+    case .tuple:
+      let count = mirror.children.count
+      target.write(count == 1 ? "(1 element)": "(\(count) elements)")
+      return
+    case .collection:
+      let count = mirror.children.count
+      target.write(count == 1 ? "1 element": "\(count) elements")
+      return
+    case .dictionary:
+      let count = mirror.children.count
+      target.write(count == 1 ? "1 key/value pair": "\(count) key/value pairs")
+      return
+    case .`set`:
+      let count = mirror.children.count
+      target.write(count == 1 ? "1 member": "\(count) members")
+      return
+    default:
+      break
+    }
+  }
+
+  if let debugPrintableObject = value as? CustomDebugStringConvertible {
+    debugPrintableObject.debugDescription.write(to: &target)
+    return
+  }
+
+  if let printableObject = value as? CustomStringConvertible {
+    printableObject.description.write(to: &target)
+    return
+  }
+
+  if let streamableObject = value as? TextOutputStreamable {
+    streamableObject.write(to: &target)
+    return
+  }
+
+  if let displayStyle = mirror.displayStyle {
+    switch displayStyle {
+    case .`class`, .`struct`:
+      // Classes and structs without custom representations are displayed as
+      // their fully qualified type name
+      target.write(_typeName(mirror.subjectType, qualified: true))
+      return
+    case .`enum`:
+      target.write(_typeName(mirror.subjectType, qualified: true))
+      if let cString = _getEnumCaseName(value),
+          let caseName = String(validatingUTF8: cString) {
+        target.write(".")
+        target.write(caseName)
+      }
+      return
+    default:
+      break
+    }
+  }
+
+  _adHocPrint_unlocked(value, mirror, &target, isDebugPrint: true)
+}
+
