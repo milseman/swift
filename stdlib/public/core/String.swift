2c617fde7bb49b2e42af65f47bfdf4508214232f
Michael Ilseman <milseman@apple.com>
2018-10-16 15:28:02 -0700

    [test] Fixup some minor test failures.
    Move withCString back to LifetimeManager.swift, which for some reason
    affects overload resolution allowing test/Constraints.closures.swift
    to pass.
    
    Fixup test/Constraints/rdar42678836.swift to mention that
    String.init(Substring) takes it shared.
    
    Stop some redundant invariant_checks from nesting, as the underlying
    object must of been checked upon construction.

--- stdlib/public/core/String.swift
+++ stdlib/public/core/String.swift
@@ -717,7 +717,6 @@ extension String {
   #else
   @usableFromInline @inline(never) @_effects(releasenone)
   internal func _invariantCheck() {
-    _guts._invariantCheck()
   }
   #endif // INTERNAL_CHECKS_ENABLED
 
@@ -1200,28 +1199,6 @@ extension String: CustomStringConvertible {
   /// assignment to create a new constant or variable equal to this string.
   @inlinable
   public var description: String { return self }
-}
-
-extension String {
-  /// Calls the given closure with a pointer to the contents of the string,
-  /// represented as a null-terminated sequence of UTF-8 code units.
-  ///
-  /// The pointer passed as an argument to `body` is valid only during the
-  /// execution of `withCString(_:)`. Do not store or return the pointer for
-  /// later use.
-  ///
-  /// - Parameter body: A closure with a pointer parameter that points to a
-  ///   null-terminated sequence of UTF-8 code units. If `body` has a return
-  ///   value, that value is also used as the return value for the
-  ///   `withCString(_:)` method. The pointer argument is valid only for the
-  ///   duration of the method's execution.
-  /// - Returns: The return value, if any, of the `body` closure parameter.
-  @inlinable // fast-path: already C-string compatible
-  public func withCString<Result>(
-    _ body: (UnsafePointer<Int8>) throws -> Result
-  ) rethrows -> Result {
-    return try _guts.withCString(body)
-  }
 }
 
 // TODO(UTF8): Move this decl back to StringIndex.swift
