e5d464220a08a1763298516e3d38a162befcd154
Michael Ilseman <michael.ilseman@gmail.com>
2024-01-07 18:28:19 -0800

    rudimentary validation shims

--- stdlib/public/core/UnicodeProcessingAPIs.swift
+++ stdlib/public/core/UnicodeProcessingAPIs.swift
@@ -1,7 +1,7 @@
 
 
 extension UTF8 {
-  public static func foo() {
+  public static func crash() {
     fatalError("running foo")
   }
 
@@ -11,6 +11,51 @@ extension UTF8 {
 
   public static func returnFalse() -> Bool {
     false
+  }
+}
+
+// HACK: quick shim for testing
+@frozen
+public struct UnsafeBufferView {
+  public var baseAddress: UnsafeRawPointer
+  public var count: Int
+}
+extension UnsafeBufferView {
+  public init(_ bufPtr: UnsafeRawBufferPointer) {
+    self.init(
+      baseAddress: bufPtr.baseAddress!, count: bufPtr.count)
+  }
+}
+
+extension Collection<UInt8> {
+  // HACK: quick shim for testing
+  public func withUnsafeBufferView<T>(
+    _ f: (UnsafeBufferView) throws -> T
+  ) rethrows -> T {
+    try Array(self).withUnsafeBytes {
+      try f(.init($0))
+    }
+  }
+}
+
+
+extension Unicode.UTF8 {
+  public enum DecodingError: Error {
+    case todo
+  }
+
+  public static func validate(
+    _ bytes: UnsafeBufferView
+  ) throws {
+    let res = bytes.baseAddress.withMemoryRebound(to: UInt8.self,  capacity: bytes.count) { ptr in
+      validateUTF8(.init(start: ptr, count: bytes.count))
+    }
+    switch res {
+
+    case .success(_): return
+    case .error(_):
+      throw UTF8.DecodingError.todo
+    }
   }
 }
 
--- test/stdlib/UnicodeProcessingAPITests.swift
+++ test/stdlib/UnicodeProcessingAPITests.swift
@@ -16,14 +16,32 @@ UnicodeProcessing.test("false") {
   expectFalse(UTF8.returnFalse())
 }
 
-UnicodeProcessing.test("true-fail") {
-  expectFalse(UTF8.returnTrue())
-}
+// UnicodeProcessing.test("true-fail") {
+//   expectFalse(UTF8.returnTrue())
+// }
 
 
-UnicodeProcessing.test("foo") {
-  UTF8.foo()
+// UnicodeProcessing.test("crash") {
+//   UTF8.crash()
+// }
+
+UnicodeProcessing.test("validate") {
+  var asciiStr = "abcdefg ./?\n\r\n123$"
+
+  var invalidUTF8 =
+    Array(asciiStr.utf8) + [0xC0, 0x0A] + Array(asciiStr.utf8)
+
+  let nonNil = try? asciiStr.utf8.withUnsafeBufferView {
+    try UTF8.validate($0)
+  }
+  expectNotNil(nonNil)
+
+  let shouldBeNil = try? invalidUTF8.withUnsafeBufferView {
+    try UTF8.validate($0)
+  }
+  expectNil(shouldBeNil)
 }
+
 
 
 runAllTests()
